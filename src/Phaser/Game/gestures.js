c988a33eac1abea0e6f677f4d010a63d064d21f2
WJXHenry <wjxhenry@gmail.com>
2019-08-16 15:55:36 -0600

    Adding origin position to gesture detection

--- src/Phaser/Game/gestures.js
+++ src/Phaser/Game/gestures.js
@@ -1,4 +1,12 @@
 /**
+ * @typedef {Object} Detection - The detected gesture and its origin
+ * @property {Gesture} gesture
+ * @property {Object} origin - The position the gesture was initiated
+ * @property {Number} origin.x
+ * @property {Number} origin.y
+ */
+
+/**
  * @typedef {Object} Gesture
  * @property {Number} SWIPE_RIGHT
  * @property {Number} SWIPE_LEFT
@@ -22,9 +30,9 @@ export const GESTURES = {
  * Register input manager for gesture detection
  * @param {Phaser.Input.InputPlugin} inputManager - The input manager that handles all input related events
  * @callback callback - The callback function that handles the detected gesture
- *  @param {Gesture} gesture - The detected gesture
- * @param {Object} options
- * @param {Number} options.swipeThreshold - The swipe threshold for the detection
+ *  @param {Detection} detection - The detected gesture
+ * @param {Object} [options]
+ * @param {Number} [options.swipeThreshold] - The swipe threshold for the detection
  */
 export function gestureDetection(inputManager, callback, options = {}) {
   inputManager.on('pointerup', pointer => {
@@ -32,6 +40,13 @@ export function gestureDetection(inputManager, callback, options = {}) {
   });
 }
 
+/**
+ * Detects a gesture based on the given Phaser Pointer object
+ * @param {Phaser.Input.Pointer} pointer - The Phaser Pointer object
+ * @param {Object} [options]
+ * @param {Number} [options.swipeThreshold] - The swipe threshold for the detection
+ * @returns {Detection}
+ */
 function detectGesture(pointer, options) {
   let swipeThreshold = options.swipeThreshold || 100;
   let deltaTime = (pointer.upTime - pointer.downTime) / 1000;
@@ -39,20 +54,39 @@ function detectGesture(pointer, options) {
   let velY = (pointer.upY - pointer.downY) / deltaTime;
   let speedX = Math.abs(velX);
   let speedY = Math.abs(velY);
+  let origin = {
+    x: pointer.downX,
+    y: pointer.downY
+  };
   if (speedX > speedY && speedX > swipeThreshold) {
     if (velX < 0) {
-      return GESTURES.SWIPE_LEFT;
+      return {
+        gesture: GESTURES.SWIPE_LEFT,
+        origin: origin
+      };
     } else {
-      return GESTURES.SWIPE_RIGHT;
+      return {
+        gesture: GESTURES.SWIPE_RIGHT,
+        origin: origin
+      };
     }
   } else if (speedY > speedX && speedY > swipeThreshold) {
     if (velY < 0) {
-      return GESTURES.SWIPE_UP;
+      return {
+        gesture: GESTURES.SWIPE_UP,
+        origin: origin
+      };
     } else {
-      return GESTURES.SWIPE_DOWN;
+      return {
+        gesture: GESTURES.SWIPE_DOWN,
+        origin: origin
+      };
     }
   }
-  return GESTURES.SINGLE_TAP;
+  return {
+    gesture: GESTURES.SINGLE_TAP,
+    origin: origin
+  };
 }
 
 // Swipe gestures without using time (speed/velocity)
